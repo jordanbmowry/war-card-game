980ed0b30cd17f98ba8cbe47cea4a31b5655b903
jordanbmowry <jordan.mowry@gmail.com>
2021-09-17 11:23:38 -0700

    added functionality to draw a new deck and then display two cards in the DOM

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const $drawCardsBtn = document.querySelector('.draw-cards-btn');
+const $drawCardsBtn = document.querySelector('.new-deck-btn');
+let deckId;
+const $drawTwoCardsBtn = document.querySelector('.draw-two-cards-btn');
+const baseURL = 'https://apis.scrimba.com/deckofcards/api/deck/';
 
-const drawCardsHandler = (event) => {
-  fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+function newDeckHandler(event) {
+  fetch(`${baseURL}/new/shuffle/`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
-};
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+      console.log(deckId);
+    });
+  $drawTwoCardsBtn.classList.remove('hidden');
+}
 
-$drawCardsBtn.addEventListener('click', drawCardsHandler);
+function drawTwoCardsHandler(event) {
+  fetch(`${baseURL}${deckId}/draw/?count=2`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector('.cards-container').innerHTML = `
+      <img src=${data.cards[0].image} alt=${data.cards[0].code}>
+      <img src=${data.cards[1].image} alt=${data.cards[1].code}>
+      `;
+    });
+}
+
+$drawCardsBtn.addEventListener('click', newDeckHandler);
+$drawTwoCardsBtn.addEventListener('click', drawTwoCardsHandler);
