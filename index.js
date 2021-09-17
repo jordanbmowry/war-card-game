5fd732300c4c58f4b0f143dc013f544821403c21
jordanbmowry <jordan.mowry@gmail.com>
2021-09-17 12:55:29 -0700

    moved dom vars to top of file and put cards into card slots

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const $drawCardsBtn = document.querySelector('.new-deck-btn');
 let deckId;
-const $drawTwoCardsBtn = document.querySelector('.draw-two-cards-btn');
 const baseURL = 'https://apis.scrimba.com/deckofcards/api/deck/';
+const $drawCardsBtn = document.querySelector('.new-deck-btn');
+const $drawTwoCardsBtn = document.querySelector('.draw-two-cards-btn');
+const $cardSlot1 = document.querySelectorAll('.card-slot')[0];
+const $cardSlot2 = document.querySelectorAll('.card-slot')[1];
 
-function newDeckHandler(event) {
+function newDeckHandler() {
   fetch(`${baseURL}/new/shuffle/`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       deckId = data.deck_id;
       console.log(deckId);
     });
   $drawTwoCardsBtn.classList.remove('hidden');
 }
 
-function drawTwoCardsHandler(event) {
+function drawTwoCardsHandler() {
   fetch(`${baseURL}${deckId}/draw/?count=2`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      document.querySelector('.cards-container').innerHTML = `
-      <img src=${data.cards[0].image} alt=${data.cards[0].code}>
-      <img src=${data.cards[1].image} alt=${data.cards[1].code}>
-      `;
+
+      $cardSlot1.innerHTML = `<img src=${data.cards[0].image} alt=${data.cards[0].code}>`;
+      $cardSlot2.innerHTML = `<img src=${data.cards[1].image} alt=${data.cards[1].code}>`;
     });
 }
 
